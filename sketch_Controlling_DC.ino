5131d02b8bd2d7aa1eafd1f203af154a6f9be739
Engbarahim <67607665+Engbarahim@users.noreply.github.com>
2020-06-30 02:28:44 +0300

    Add files via upload

---
+++ sketch_Controlling_DC.ino
@@ -0,0 +1,63 @@
+//L293D
+
+//Motor A
+const int motorPin1  = 7;  // IN1 of L293
+const int motorPin2  = 6;  // IN2 10 of L293
+
+//Motor B
+const int motorPin3  = 5; // IN3 of L293
+const int motorPin4  = 4;  // IN4 of L293
+
+
+void setup(){
+ 
+    //Set pins as outputs
+    pinMode(motorPin1, OUTPUT);
+    pinMode(motorPin2, OUTPUT);
+    pinMode(motorPin3, OUTPUT);
+    pinMode(motorPin4, OUTPUT);
+      
+}
+
+
+void loop(){
+
+   //Motor Control - Motor A: motorPin1,motorpin2 & Motor B: motorpin3,motorpin4
+
+    //This code  will turn Motor A clockwise for 2 sec.
+    analogWrite(motorPin1, 180);
+    analogWrite(motorPin2, 0);
+    analogWrite(motorPin3, 180);
+    analogWrite(motorPin4, 0);
+    delay(2000); 
+    
+    //This code will turn Motor A counter-clockwise for 2 sec.
+    analogWrite(motorPin1, 0);
+    analogWrite(motorPin2, 180);
+    analogWrite(motorPin3, 0);
+    analogWrite(motorPin4, 180);
+    delay(2000);
+    
+    //This code will turn Motor B clockwise for 2 sec.
+    analogWrite(motorPin1, 0);
+    analogWrite(motorPin2, 180);
+    analogWrite(motorPin3, 180);
+    analogWrite(motorPin4, 0);
+    delay(2000); 
+    
+    //This code will turn Motor B counter-clockwise for 2 sec.
+    analogWrite(motorPin1, 180);
+    analogWrite(motorPin2, 0);
+    analogWrite(motorPin3, 0);
+    analogWrite(motorPin4, 180);
+    delay(2000);    
+    
+    //And this code will stop motors for 10 sec.
+    analogWrite(motorPin1, 0);
+    analogWrite(motorPin2, 0);
+    analogWrite(motorPin3, 0);
+    analogWrite(motorPin4, 0);
+    delay(10000)
+  
+
+}
